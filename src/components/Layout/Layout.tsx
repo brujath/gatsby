c9393909cc012f3d5dd1a362e2266fa10e41989e
Bru <bru@014AMBBLR-Brujat-U.local>
2020-06-29 17:43:36 +0530

    Configure storybook

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import Header from 'components/Header';
 import Footer from 'components/Footer';
 import Navbar from 'components/Navbar';
 
-import './layout.scss';
+import './Layout.scss';
 
 type Props = {
   children: ReactNode;
